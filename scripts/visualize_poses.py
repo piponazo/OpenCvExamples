1a27b4f0aaab0d2b163c4357954797da956cf1f6
Luis Diaz Mas <luis.diaz-mas@hexagon.com>
2018-08-10 15:25:16 +0200

    Add python script to compare KITTY ground-truth with computed poses

---
+++ scripts/visualize_poses.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import argparse
+import sys
+import matplotlib
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+
+class _Exit:
+    Success = 0
+    Failure = 1
+
+
+def parse_poses_file(path):
+    with open(path) as f:
+        lines = f.readlines()
+
+    xs = []
+    zs = []
+    for line in lines:
+        fields = line.split(' ')
+        x = float(fields[3])
+        y = float(fields[7])
+        z = float(fields[11])
+        xs.append(x)
+        zs.append(z)
+
+    return xs, zs
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Compare ground truth poses with the computed ones.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-g', '--ground', help='Location of ground truth poses file', required=True)
+    parser.add_argument('-c', '--computed', help='Location of computed poses file', required=True)
+
+    args = parser.parse_args()
+
+    groundTruthPath = Path(args.ground).resolve()
+    computedPath = Path(args.computed).resolve()
+
+    print(groundTruthPath)
+    print(computedPath)
+
+    gX, gZ = parse_poses_file(groundTruthPath)
+    cX, cZ = parse_poses_file(computedPath)
+
+    fig, ax = plt.subplots()
+
+    ax.plot(gX, gZ, label='GT')         # GT data
+    ax.plot(gX[0], gZ[0], 'ro')         # GT first point in red
+    ax.plot(gX[-1], gZ[-1], 'go')       # GT last point in green
+
+    ax.plot(cX, cZ, label='Computed')   # computed data
+    ax.plot(cX[0], cZ[0], 'yo')         # computed first point in red
+    ax.plot(cX[-1], cZ[-1], 'bo')       # computed last point in green
+
+    ax.set(xlabel='x coordinate', ylabel='z coordinate',
+           title='KITTY Odometry comparison (GT and computed poses)')
+    ax.grid()
+    ax.legend()
+    plt.show()
+
+    return _Exit.Success
+
+
+if __name__ == "__main__":
+    sys.exit(main())
